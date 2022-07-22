578ea2bf8612d33cb31bdbbe943f92c3149809d4
Kristine Jecha <98754172+kriskaykj@users.noreply.github.com>
2022-07-22 11:22:00 +0200

    Update catgen.r

--- catgen.r
+++ catgen.r
@@ -3,27 +3,30 @@ setwd(paste(YOURDIR, "/sam/cut", sep=""))
 library(dplyr)
 library(ggplot2)
 
+#create list of sample IDs based on file names
 reads <- as.data.frame(list.files(path = (paste(YOURDIR, "/sam/cut", sep=""))))
 reads <- as.data.frame(sub(".sam.*", "", reads[,1]))
 reads <- gsub(".*_","",reads[,1])
 
-#
+#confirm sample ID in read variable
 head(reads)
-#
+
+#for each read...
 for (row in  reads){
 setwd(paste(YOURDIR, "/sam/cut", sep=""))
   print(row)
   sam <- read.table(paste("catgen_", row,".sam", sep = ""), fill = T, row.names=NULL)
 print(head(sam, n=1))
-
+#create column names based on .sam file output
   colnames(sam) <- c("qname","flag","scaf","pos","mapq","cigar",
                      "mrnm","mpos","isize","seq","qual" )
   sam <- sam[!is.na(sam$scaf),]
   sam = subset(sam, select = c("qname","flag","scaf","pos","mapq","cigar",
                                "mrnm","mpos","isize","seq","qual") )
+#filter out reads that had a mapping quality less than 30
   sam.filter <- subset(sam, mapq >= 30) 
 	print("filter out low qual: done")
-
+#label each alignment with the species it mapped to 			!CHANGE SPECIES NAMES!
   sam.filter.sp <- sam.filter %>%
     mutate(spec = case_when(
       startsWith(sam.filter$scaf, "Bacillus_rossius") ~ "Bacillus_rossius" , 
@@ -50,7 +53,7 @@ print(head(sam, n=1))
   
   print("add species names: done")
  print(colnames(sam.filter.sp))
-  
+  #create file to check that species names were added
 uniques <- (sam.filter.sp[!duplicated(sam.filter.sp$spec), ])
 write.table(uniques, paste("sam.filter_",row ,".txt", sep=""), 
 append = FALSE, sep = " ", dec = ".",
@@ -59,7 +62,7 @@ row.names = TRUE, col.names = TRUE)
 print("wrote out file: done")
 
 setwd("YOURDIR")
-
+#create mapplot for the sample, output at .jpeg
  p<- ggplot(sam.filter.sp, aes(x=spec, fill=spec)) + 
     geom_bar(aes(y = (..count..)/sum(..count..))) +
    geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::scientific((..count..)/sum(..count..))),
