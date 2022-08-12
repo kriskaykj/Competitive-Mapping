aa4b5e32c2582536f5f80e8ab177d742c0fc8772
Kristine Jecha <98754172+kriskaykj@users.noreply.github.com>
2022-08-12 14:25:25 +0200

    Update filterReads.r

--- filterReads.r
+++ filterReads.r
@@ -70,3 +70,20 @@ setwd(YOURDIR)
 write.table(formica, "targetReads.txt", 
             append = FALSE, sep = " ", dec = ".",
             row.names = F , col.names = TRUE, quote = F)
+
+  
+fr <- read.table("targetReads.txt", header = F)
+#Split up targetReads.txt table by column, creating an individual file for each sample with all correctly mapping reads
+for (i in fr){
+  SAMP <- i[1]
+    i <- na.omit(i)
+    i <- paste(i,"/1", sep="")
+  outfile <- as.data.frame(i)
+  outfile = outfile[-1,]
+print(SAMP)
+setwd(paste(YOURDIR, "/IDS", sep=""))
+  write.table(outfile, paste(SAMP,"_ID.txt", sep=""), 
+              append = FALSE, sep = " ", dec = ".",
+              row.names = F , col.names = F, quote = F)
+}
+
