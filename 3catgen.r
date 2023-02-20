bc2cd7f6e2614286bce1854d0894748f87cdd996
Kristine Jecha <98754172+kriskaykj@users.noreply.github.com>
2023-02-20 13:47:00 +0100

    Update 3catgen.r

--- 3catgen.r
+++ 3catgen.r
@@ -1,5 +1,5 @@
 
-YOURDIR <-"/scratch/kjecha/ants/catgen"
+YOURDIR <-"/work/FAC/FBM/DEE/tschwand/operation_fourmis/kjecha/AllGenusCompetitive-Mapping/Myrm"
 setwd(paste(YOURDIR, "/sam/cut", sep=""))
 library(dplyr)
 library(ggplot2)
@@ -44,27 +44,14 @@ for (row in  reads){
   #label each alignment with the species it mapped to 			!CHANGE SPECIES NAMES!
     sam.filter.sp <- sam.filter %>%
     mutate(spec = case_when(
-      startsWith(sam.filter$scaf, "Bacillus_rossius") ~ "Bacillus_rossius" , 
-      startsWith(sam.filter$scaf, "Camponotus_fallax") ~ "Camponotus_fallax" ,
-      startsWith(sam.filter$scaf, "Lasius_alienus") ~ "Lasius_alienus" , 
-      startsWith(sam.filter$scaf, "Lasius_cf_spathepus") ~ "Lasius_cf_spathepus" ,
-      startsWith(sam.filter$scaf, "Lasius_flavus_s1") ~ "Lasius_flavus_s1" , 
-      startsWith(sam.filter$scaf, "Lasius_flavus_s2") ~ "Lasius_flavus_s2" ,
-      startsWith(sam.filter$scaf, "Lasius_fuliginosus_s1") ~ "Lasius_fuliginosus_s1" , 
-      startsWith(sam.filter$scaf, "Lasius_fuliginosus_s2") ~ "Lasius_fuliginosus_s2" ,
-      startsWith(sam.filter$scaf, "Lasius_neglectus") ~ "Lasius_neglectus" , 
-      startsWith(sam.filter$scaf, "Lasius_niger") ~ "Lasius_niger" ,
-      startsWith(sam.filter$scaf, "Linepithema_humile") ~ "Linepithema_humile" , 
-      startsWith(sam.filter$scaf, "Messor_barbarusFlye") ~ "Messor_barbarusFlye" ,
-      startsWith(sam.filter$scaf, "Messor_barbarus") ~ "Messor_barbarus" , 
-      startsWith(sam.filter$scaf, "Messor_capitatus") ~ "Messor_capitatus" ,
-      startsWith(sam.filter$scaf, "Pseudolasius_sp") ~ "Pseudolasius_sp" , 
-      startsWith(sam.filter$scaf, "Solenopsis_fugax") ~ "Solenopsis_fugax" ,
-      startsWith(sam.filter$scaf, "Solenopsis_geminata") ~ "Solenopsis_geminata" , 
-      startsWith(sam.filter$scaf, "Solenopsis_invicta") ~ "Solenopsis_invicta" ,
-      startsWith(sam.filter$scaf, "Timema_douglasi") ~ "Timema_douglasi" , 
-      startsWith(sam.filter$scaf, "Triticum_aestivum") ~ "Triticum_aestivum" ,
-    ))
+      startsWith(sam.filter$scaf, "Camponotus_floridanus") ~ "Camponotus_floridanus" , 
+      startsWith(sam.filter$scaf, "Formica_selysi") ~ "Formica_selysi" ,
+      startsWith(sam.filter$scaf, "Leptothorax_acervorum") ~ "Leptothorax_acervorum" , 
+      startsWith(sam.filter$scaf, "Myrmica_rubra") ~ "Myrmica_rubra" ,
+      startsWith(sam.filter$scaf, "Tapinoma_erraticum") ~ "Tapinoma_erraticum" , 
+      startsWith(sam.filter$scaf, "Temnothorax_unifasciatus") ~ "Temnothorax_unifasciatus" ,
+      startsWith(sam.filter$scaf, "Tetramorium_immigrans") ~ "Tetramorium_immigrans" , 
+      startsWith(sam.filter$scaf, "Lasius_niger") ~ "Lasius_niger"  ))
   
   print("add species names: done")
   print(colnames(sam.filter.sp))
@@ -72,27 +59,18 @@ for (row in  reads){
   #create file to check that species names were added
   uniques <- (sam.filter.sp[!duplicated(sam.filter.sp$spec), ])
 
+  #create mapplot for the sample, output at .jpeg   !UNCOMMENT IF YOU WANT MAPPLOT FOR EACH SAMPLE!
 
- #create file to check that species names were added
-  #write.table(uniques, paste("sam.filter_",row ,".txt", sep=""), 
-  #            append = FALSE, sep = " ", dec = ".",
-   #           row.names = TRUE, col.names = TRUE) 	
-  
-  print("wrote out file: done")
-  
- setwd(paste(YOURDIR, "/mapplots", sep=""))		
-  
-  #create mapplot for the sample, output at .jpeg
-   p<- ggplot(sam.filter.sp, aes(x=spec, fill=spec)) + 
-      geom_bar(aes(y = (..count..)/sum(..count..))) +
-     geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::scientific((..count..)/sum(..count..))),
-               stat = "count", vjust = 0.5, hjust=0, angle=90, size=3) + 
-      labs(title=(paste("Maps per species. read:",row)), x ="species", y = "proportion")+ ylim(0,1.01)+
-      theme(axis.text.x = element_text(angle = 45, vjust = 1.0, hjust=1))
-   jpeg(paste("mapplot_",row ,".jpeg", sep=""),width = 480, height = 480, quality = 75)
-   print(p)
-      dev.off() 
-  print("pic made:done")
+#   p<- ggplot(sam.filter.sp, aes(x=spec, fill=spec)) + 
+#      geom_bar(aes(y = (..count..)/sum(..count..))) +
+#     geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::scientific((..count..)/sum(..count..))),
+#               stat = "count", vjust = 0.5, hjust=0, angle=90, size=3) + 
+ #     labs(title=(paste("Maps per species. read:",row)), x ="species", y = "proportion")+ ylim(0,1.01)+
+ #     theme(axis.text.x = element_text(angle = 45, vjust = 1.0, hjust=1))
+ #  jpeg(paste("mapplot_",row ,".jpeg", sep=""),width = 480, height = 480, quality = 75)
+ #  print(p)
+ #     dev.off() 
+ # print("pic made:done")
   
 
   tempdf<- data.frame()
